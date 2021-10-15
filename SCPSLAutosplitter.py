e5f8bb503962d4f8fc9d6f3a5b99291e14048423
crd716@gmail.com <crd716@gmail.com>
2021-10-15 18:55:05 -0500

    orang detection

--- SCPSLAutosplitter.py
+++ SCPSLAutosplitter.py
@@ -1,1 +1,25 @@
+from pynput.keyboard import Key, Controller
+from PIL import ImageGrab#, ImageDraw
+from time import sleep
 
+while True:
+	screen = ImageGrab.grab()
+	#screen.show()
+	# 255, 142, 0 is the d class rgb values
+	width, height = screen.size
+
+	#uncomment these and the import imagedraw so you can sorta see where it's targeting
+	#img1 = ImageDraw.Draw(screen)
+	#img1.point([width/2, height/2.5], (0,255,0))
+	#screen.show()
+	
+
+	r, g, b = screen.getpixel((width/2, height/2.5)) #modify x and y if it isn't working for you
+
+	print(r, g, b)
+	if (r in range(230,255)) & (g in range(135, 150)) & (b in range(0,5)): #play around with these ranges.
+		print("ORANG DETECTED!")
+		sleep(1000)
+	else:
+		print("nope")
+		sleep(200)
